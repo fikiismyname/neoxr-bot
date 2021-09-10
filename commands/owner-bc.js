bcd1af3a85e5d6a74902895e4cadd422fa411c1b
neoxr <neoxr@null.net>
2021-09-10 20:20:29 +0700

    UPDATE

--- commands/owner-bc.js
+++ commands/owner-bc.js
@@ -12,7 +12,7 @@ exports.run = {
 				await conn.relayWAMessage(msg)
 				await _func.delay(2500)
 			} m.reply(`*Successfully broadcast to all chats.*`)
-		} else if (command == 'bcgc') {
+		} else if (command == 'bcpc') {
 			for (let id of chats) {
 				if (id.endsWith('s.whatsapp.net')) {
 					let msg = conn.cMod(id, conn.prepareMessageFromContent(id, {[c.toJSON ? q.mtype : MessageType.extendedText]: c.toJSON ? c.toJSON() : { text: c || '' }}, {contextInfo: { mentionedJid: ['0@c.us'] }, quoted: {key: {fromMe: false, participant: `0@s.whatsapp.net`, ...(id ? { remoteJid: 'status@broadcast' } : {}) }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": 'B R O A D C A S T', "jpegThumbnail": await _func.buffer('./media/images/thumb.jpg')} } }}), text || q.text)
@@ -20,7 +20,7 @@ exports.run = {
 					await _func.delay(2500)
 	    		}
 			} m.reply(`*Successfully broadcast personal chat.*`)
-		} else if (command == 'bcpc') {
+		} else if (command == 'bcgc') {
 			for (let id of chats) {
 				if (id.endsWith('g.us')) {
 					let msg = conn.cMod(id, conn.prepareMessageFromContent(id, {[c.toJSON ? q.mtype : MessageType.extendedText]: c.toJSON ? c.toJSON() : { text: c || '' }}, {contextInfo: { mentionedJid: ['0@c.us'] }, quoted: {key: {fromMe: false, participant: `0@s.whatsapp.net`, ...(id ? { remoteJid: 'status@broadcast' } : {}) }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": 'B R O A D C A S T', "jpegThumbnail": await _func.buffer('./media/images/thumb.jpg')} } }}), text || q.text)
