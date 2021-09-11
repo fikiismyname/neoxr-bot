5220fa6eaefe154d73b26b74a3d080c0ac1ab707
neoxr <neoxr@null.net>
2021-09-11 22:52:30 +0700

    add auto reconnecting

--- commands/user-limit.js
+++ commands/user-limit.js
@@ -5,7 +5,7 @@ exports.run = {
 		let user = global.db.users[m.sender]
 		let owner = global.db.setting.owner
 		conn.updatePresence(m.chat, Presence.composing)
-		if (owner.includes(m.sender.split`@`[0])) return m.reply(`*Your limit is Unlimited.*`)
+		if (owner.includes(m.sender.split`@`[0]) || user.premium) return m.reply(`*Your limit is Unlimited.*`)
 		if (user.limit == 0) return conn.sendButtonLoc(m.chat, await _func.buffer(global.db.setting.cover), `*Sorry @${m.sender.split`@`[0]}, you don't have a limit, please exchange it with your points first.*`, global.footer, 'EXCHANGE', `${isPrefix}exchange 1`, m)
 		m.reply(`*Your have ${user.limit} limits.*`)
 	},
